065e5c6d307bfd10525f6ab95e4eb91e45166e0e
anto95240 <richard33antoine@gmail.com>
2024-12-04 12:22:28 +0100

    Organisation

---
+++ app/pages/Home.tsx
@@ -0,0 +1,7 @@
+import Nav from "../components/Navbar"
+
+export default function Home() {
+  return(
+    <Nav/>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { title } from "process";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -7,10 +8,19 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily:{
+      title: ["var(--font-title)"],
+      text: ["var(--font-text)"],
+    },
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'light-blue': '#639DF0',
+        'light-green': '#24F4BA',
+        'green-blue': '#96ECE1',
+        'blue-darkBlue': '#36A7F8',
+        'dark-blue': '#1D2B64',
       },
     },
   },
