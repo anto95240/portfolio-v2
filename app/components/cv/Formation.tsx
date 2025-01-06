b2cc2d01f11461f3e6d03da772f080a16068d6ee
anto95240 <richard33antoine@gmail.com>
2025-01-06 16:31:24 +0100

    START Formation

---
+++ app/components/cv/Formation.tsx
@@ -0,0 +1,12 @@
+import formationData from '../../data/cv_formation.json';
+
+export default function Formation() {
+
+  return (
+    <div className="w-8/12 flex flex-col">
+      <hr className="bg-black w-full my-10 h-[2px] border-none rounded" />
+      <h1 className="text-2xl mb-10 text-center">FORMATION</h1>
+
+    </div>
+  );
+}
