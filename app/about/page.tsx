5ea0bc4b5bfb219709aa298785e34db0246e92e7
anto95240 <richard33antoine@gmail.com>
2025-01-07 17:55:41 +0100

    UPDATE About

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -18,21 +18,21 @@ export default function About() {
         <div className="flex flex-col lg:flex-row items-center lg:items-start gap-10 lg:gap-20 w-full">
           <div>
             <Image
-              className="mb-10"
+              className="mb-10 lg:w-[450px] w-[230px]"
               src="/images/photo.svg"
               alt="Photo"
-              width={230}
+              width={450}
               height={310}
             />
           </div>
           <div className="flex flex-col">
-            <div className="flex flex-col justify-center text-center lg:text-left max-w-lg">
+            <div className="flex flex-col justify-center text-center mx-auto lg:text-left max-w-lg">
               <p className="text-base leading-relaxed">
                 Hello, moi c’est <b>Antoine</b> ! <b>Etudiant</b> en <b>informatique</b>. Je suis actuellement un cursus de 5 ans en informatique et me passionne de plus en plus pour le <b>domaine du web</b>.
               </p>
             </div>
-
-            <div className="grid grid-cols-3 gap-6 mt-10">
+            <div className="w-full">
+            <div className="grid grid-cols-3 gap-2 mt-5 justify-items-center lg:justify-items-start">
               <div className="w-24 h-24 bg-blue-300 rounded-full flex items-center justify-center text-lg text-black shadow-md">
                 Loisir 1
               </div>
@@ -42,13 +42,19 @@ export default function About() {
               <div className="w-24 h-24 bg-blue-300 rounded-full flex items-center justify-center text-lg text-black shadow-md">
                 Loisir 3
               </div>
-              <div className="w-24 h-24 bg-blue-300 rounded-full flex items-center justify-center text-lg text-black shadow-md">
+            </div>
+
+            <div className="grid grid-cols-7 mt-5 gap-2 justify-items-center lg:justify-items-start">
+              <div className="w-24 h-24 bg-blue-300 rounded-full flex items-center justify-center text-lg text-black shadow-md col-start-3 lg:col-start-2">
                 Loisir 4
               </div>
-              <div className="w-24 h-24 bg-blue-300 rounded-full flex items-center justify-center text-lg text-black shadow-md">
+              <div className="w-24 h-24 bg-blue-300 rounded-full flex items-center justify-center text-lg text-black shadow-md col-start-5 lg:col-end-4 lg:ml-10">
                 Loisir 5
               </div>
             </div>
+            </div>
+            
+
           </div>          
         </div>
 
