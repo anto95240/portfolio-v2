2141f9384d2984b58abcd05f174bc5fd41bcd378
anto95240 <richard33antoine@gmail.com>
2025-01-08 13:11:04 +0100

    UPDATE OutilsHome

--- app/components/OutilsHome.tsx
+++ app/components/OutilsHome.tsx
@@ -11,35 +11,37 @@ export default function OutilHome() {
     <div className="w-8/12 flex flex-col">
       <hr className="bg-black w-full my-10 h-[2px] border-none rounded" />
       <h1 className="mb-5 text-center">Mes outils</h1>
-    <div className="bg-green-outil w-full max-w-2xl mx-auto p-3 rounded-xl">
-      {/* Conteneur principal */}
-      <div className="flex flex-col md:flex-row md:items-center md:justify-between">
-        {/* Grille des outils */}
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-5">
-          {tools.map((tool) => (
-            <div
-              key={tool.id}
-              className="flex items-center bg-blue-outil px-2 py-3 rounded-md shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] justify-between"
-            >
-              <Image
-                src={tool.images}
-                alt={`${tool.title} Logo`}
-                width={30}
-                height={30}
-              />
-              <p className="md:text-lg text-base text-white ml-2 font-text">{tool.title}</p>
-            </div>
-          ))}
-        </div>
+      <div className="bg-green-outil w-full max-w-2xl mx-auto p-3 rounded-xl">
+        {/* Conteneur principal */}
+        <div className="flex flex-col md:flex-row md:items-center md:justify-between">
+          {/* Grille des outils */}
+          <div className="grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-5">
+            {tools.map((tool) => (
+              <div
+                key={tool.id}
+                className="flex items-center bg-blue-outil px-2 py-3 rounded-md shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] justify-between"
+              >
+                <Image
+                  src={tool.images}
+                  alt={`${tool.title} Logo`}
+                  width={30}
+                  height={30}
+                />
+                <p className="md:text-lg text-base text-white ml-2 font-text">{tool.title}</p>
+              </div>
+            ))}
+          </div>
 
-        {/* Bouton VOIR PLUS */}
-        <div className="mt-6 md:mt-0 md:ml-6 text-center">
-          <Link href="/cv" className="bg-gradient-to-r from-light-blue via-light-green to-light-blue text-black md:text-lg py-3 px-4 text-base rounded-md shadow-[4px_4px_10px_0_rgba(0,0,0,0.5)] transition-transform transform hover:scale-105">
-            VOIR PLUS
-          </Link>
+          {/* Bouton VOIR PLUS */}
+          <div className="mt-6 md:mt-0 md:ml-6 text-center">
+            <button className="bg-gradient-to-r from-light-blue via-light-green to-light-blue text-black md:text-lg py-3 px-4 text-base rounded-md shadow-[4px_4px_10px_0_rgba(0,0,0,0.5)] transition-transform transform hover:scale-105">
+              <Link href="/cv">
+                VOIR PLUS
+              </Link>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
