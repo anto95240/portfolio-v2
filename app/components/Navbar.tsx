7371e887c3258c023cea6bb25c62d48c11b81611
Antoine RICHARD <richard33antoine@gmail.com>
2025-08-08 18:35:32 +0200

    UPDATE cv + remove anim saison

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,10 @@
 "use client";
 
 import { useState, useEffect, useRef } from "react";
-import gsap from "gsap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse, faCircleInfo, faFile, faFileCircleCheck, faChevronDown, faChevronUp, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
 import Link from "next/link";
-import ClientOnlyNavbar from "./ClientOnlyNavbar";
-
-type Season = "spring" | "summer" | "fall" | "winter";
 
 const useIsClient = () => {
   const [isClient, setIsClient] = useState(false);
@@ -20,315 +16,99 @@ const useIsClient = () => {
   return isClient;
 };
 
-const getCurrentSeason = () => {
-  const now = new Date();
-  const year = now.getFullYear();
-
-  const springEquinox = new Date(year, 2, 20);
-  const summerSolstice = new Date(year, 5, 21);
-  const fallEquinox = new Date(year, 8, 22);
-  const winterSolstice = new Date(year, 11, 21);
-
-  if (now >= springEquinox && now < summerSolstice) return "spring";
-  if (now >= summerSolstice && now < fallEquinox) return "summer";
-  if (now >= fallEquinox && now < winterSolstice) return "fall";
-  return "winter";
-};
-
 export default function Navbar({ isMenuOpen, setIsMenuOpen }: { isMenuOpen: boolean; setIsMenuOpen: React.Dispatch<React.SetStateAction<boolean>> }) {
   const [isProjectOpen, setIsProjectOpen] = useState(false);
-  const [season, setSeason] = useState<Season | null>(null);
-  const navRef = useRef<HTMLDivElement>(null);
-
-  const snowflakesRef = useRef<(HTMLDivElement | null)[]>([]);
-  const fallingLeavesRef = useRef<(HTMLDivElement | null)[]>([]);
-  const bloomingPlantsRef = useRef<(HTMLDivElement | null)[]>([]);
-  const palmTreeRef = useRef<(HTMLDivElement | null)[]>([]);
 
   const isClient = useIsClient();
-
-  useEffect(() => {
-    setSeason(getCurrentSeason());
-    const interval = setInterval(() => {
-      setSeason(getCurrentSeason());
-    }, 86400000);
-    return () => clearInterval(interval);
-  }, []);
-
-  const generateRandoms = (count: number) => {
-    const arr: number[] = [];
-    for (let i = 0; i < count; i++) {
-      arr.push(Math.random());
-    }
-    return arr;
-  };
-
-  useEffect(() => {
-    if (!season || !navRef.current) return;
-  
-    const navbarHeight = navRef.current.offsetHeight;
-
-    const clearChildren = () => {
-      snowflakesRef.current.forEach(el => el?.remove());
-      fallingLeavesRef.current.forEach(el => el?.remove());
-      bloomingPlantsRef.current.forEach(el => el?.remove());
-      palmTreeRef.current.forEach(el => el?.remove());
-
-      snowflakesRef.current = [];
-      fallingLeavesRef.current = [];
-      bloomingPlantsRef.current = [];
-      palmTreeRef.current = [];
-    };
-
-    clearChildren();
-
-    // Créer et animer les éléments en fonction de la saison
-    if (season === "winter") {
-      const numFlakes = 20; 
-      const randoms = generateRandoms(numFlakes * 5);
-
-      for (let i = 0; i < numFlakes; i++) {
-        const snowflake = document.createElement("img");
-        snowflake.src = "/images/snowflakes.webp"; // Chemin vers l'image de flocon
-        snowflake.classList.add("snowflake");
-        snowflake.style.position = "absolute";
-        snowflake.style.top = `${randoms[i] * -50}px`;
-        snowflake.style.left = `${randoms[i] * 90}%`;
-        snowflake.style.width = "20px";
-        snowflake.style.height = "20px";
-        snowflake.style.opacity = "0.6";
-
-        snowflakesRef.current.push(snowflake);
-        navRef.current?.appendChild(snowflake);
-
-        // Animation continue
-        gsap.to(snowflake, {
-          y: navbarHeight,
-          duration: randoms[i + 40] * 5 + 5,
-          x: `+=${randoms[i + 60] * 100 - 50}%`,
-          repeat: -1,
-          yoyo: false,
-          ease: "power1.inOut",
-          delay: randoms[i + 80] * 5,
-          onRepeat: () => {
-            gsap.set(snowflake, { top: `${randoms[i] * -50}px` });
-          },
-        });
-      }
-    } else if (season === "fall") {
-      const numLeaves = 20;
-      const randoms = generateRandoms(numLeaves * 5);
-
-    for (let i = 0; i < numLeaves; i++) {
-      const leaf = document.createElement("img");
-      leaf.src = "/images/falling-leaves.webp"; // Chemin vers l'image de feuille
-      leaf.classList.add("leaf");
-      leaf.style.position = "absolute";
-      leaf.style.top = `${randoms[i] * -50}px`;
-      leaf.style.left = `${randoms[i] * 90}%`;
-      leaf.style.width = "20px";
-      leaf.style.height = "20px";
-      leaf.style.opacity = "0.6";
-      fallingLeavesRef.current.push(leaf);
-      navRef.current?.appendChild(leaf);
-
-      gsap.to(leaf, {
-        y: navbarHeight,
-        duration: randoms[i + 40] * 5 + 5,
-        x: `+=${randoms[i + 60] * 100 - 50}%`,
-        repeat: -1,
-        yoyo: false,
-        ease: "power1.inOut",
-        delay: randoms[i + 80] * 5,
-        onRepeat: () => {
-          gsap.set(leaf, { top: `${randoms[i] * -50}px` });
-        },
-      });
-    }
-    } else if (season === "spring") {
-      if (bloomingPlantsRef.current.length === 0) {
-        const numFlowers = 15;
-        const randoms = generateRandoms(numFlowers * 5);
-    
-        for (let i = 0; i < numFlowers; i++) {
-          setTimeout(() => { // Apparition progressive
-            const flower = document.createElement("img");
-            flower.src = "/images/blooming-plants.webp";
-            flower.classList.add("flower");
-            flower.style.position = "absolute";
-            flower.style.bottom = "0px";
-            flower.style.left = `${randoms[i + 10] * 90}%`;
-            flower.style.width = "0px";
-            flower.style.height = "0px";
-            flower.style.opacity = "0";
-
-            flower.style.zIndex = `${Math.floor(randoms[i + 15] * 5)}`;
-            flower.style.filter = `hue-rotate(${randoms[i] * 40 - 20}deg) brightness(${randoms[i + 5] * 0.4 + 0.8})`;
-
-            bloomingPlantsRef.current.push(flower);
-            navRef.current?.appendChild(flower);
-    
-            const targetSize = randoms[i + 20] * 30 + 50;
-
-            gsap.fromTo(
-              flower,
-              {
-                scale: 0,
-                opacity: 0,
-                // y: 30,
-                rotation: randoms[i + 25] * 60 - 30
-              },
-              {
-                width: `${targetSize}px`,
-                height: `${targetSize}px`,
-                scale: 1,
-                opacity: 0.8,
-                y: 0,
-                rotation: 0,
-                duration: 1.8,
-                ease: "elastic.out(1, 0.5)"
-              }
-            );
-
-            gsap.to(flower, {
-              x: "+=5",
-              duration: 2 + randoms[i + 30],
-              yoyo: true,
-              repeat: -1,
-              ease: "sine.inOut"
-            });
-
-          }, i * 400);
-        }
-      }
-    } else if (season === "summer") {
-      const numPalms = 15;
-      const randoms = generateRandoms(numPalms * 4);
-
-      navRef.current.style.perspective = "800px";
-      for (let i = 0; i < numPalms; i++) {
-        const palmTree = document.createElement("img");
-        palmTree.src = "/images/palm-tree.webp";
-        palmTree.classList.add("palm-tree");
-
-        palmTree.style.position = "absolute";
-        palmTree.style.top = `${randoms[i] * 80}%`;
-        palmTree.style.left = `${randoms[i + 10] * 90}%`;
-        palmTree.style.width = "40px";
-        palmTree.style.height = "60px";
-        palmTree.style.opacity = "0.7";
-        palmTree.style.zIndex = "2";
-
-        palmTree.style.transformStyle = "preserve-3d";
-        palmTree.style.transformOrigin = "bottom center";
-
-        palmTreeRef.current.push(palmTree);
-        navRef.current.appendChild(palmTree);
-
-        gsap.to(palmTree, {
-          rotateY: randoms[i + 20] * 100 - 15,
-          duration: randoms[i + 30] * 3 + 2,
-          repeat: -1,
-          yoyo: true,
-          ease: "sine.inOut"
-        });
-      }
-    }
-
-  }, [season]);
   
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
   const toggleProjectList = () => setIsProjectOpen(!isProjectOpen);
 
-  if (!season) return null;
   if (!isClient) return null;
 
   return (
-    <ClientOnlyNavbar>
-      <div className="relative h-screen">
+    <div className="relative h-screen">
+      <button
+        aria-label="Ouvrir le menu"
+        className={`absolute top-5 left-5 text-2xl z-50 lg:hidden ${isMenuOpen ? "hidden" : "block"}`}
+        onClick={toggleMenu}
+      >
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+
+      <nav
+        className={`fixed z-40 top-0 left-0 h-screen bg-gradient-to-r from-light-blue to-light-green shadow-[0_10px_10px_0_rgba(0,0,0,0.75)] p-5 transition-transform duration-300 flex flex-col items-center
+        ${isMenuOpen ? "translate-x-0 lg:pointer-events-none pointer-events-auto" : "-translate-x-full lg:pointer-events-auto pointer-events-none"} lg:translate-x-0 lg:relative lg:w-4/5 w-3/5 sm:w-2/6`}
+      >
         <button
-          aria-label="Ouvrir le menu"
-          className={`absolute top-5 left-5 text-2xl z-50 lg:hidden ${isMenuOpen ? "hidden" : "block"}`}
+          aria-label="Fermer le menu"
+          className={`absolute top-5 right-5 text-2xl lg:hidden ${isMenuOpen ? "block" : "hidden"}`}
           onClick={toggleMenu}
         >
-          <FontAwesomeIcon icon={faBars} />
+          <FontAwesomeIcon icon={faTimes} />
         </button>
 
-        <nav
-          ref={navRef}
-          className={`fixed z-40 top-0 left-0 h-screen bg-gradient-to-r from-light-blue to-light-green shadow-[0_10px_10px_0_rgba(0,0,0,0.75)] p-5 transition-transform duration-300 flex flex-col items-center
-          ${isMenuOpen ? "translate-x-0 lg:pointer-events-none pointer-events-auto" : "-translate-x-full lg:pointer-events-auto pointer-events-none"} lg:translate-x-0 lg:relative lg:w-4/5 w-3/5 sm:w-2/6`}
-        >
-          <button
-            aria-label="Fermer le menu"
-            className={`absolute top-5 right-5 text-2xl lg:hidden ${isMenuOpen ? "block" : "hidden"}`}
-            onClick={toggleMenu}
-          >
-            <FontAwesomeIcon icon={faTimes} />
-          </button>
+        <Image className="mb-10" src="/images/logo.svg" alt="Logo" width={75} height={62} />
 
-          <Image className="mb-10" src="/images/logo.svg" alt="Logo" width={75} height={62} />
+        <ol className="flex flex-col gap-8">
+          <li className="flex items-center gap-4">
+            <FontAwesomeIcon icon={faHouse} className="text-2xl" />
+            <Link href="/" className="relative group z-50">
+              Accueil
+              <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
+            </Link>
+          </li>
 
-          <ol className="flex flex-col gap-8">
-            <li className="flex items-center gap-4">
-              <FontAwesomeIcon icon={faHouse} className="text-2xl" />
-              <Link href="/" className="relative group z-50">
-                Accueil
+          <li>
+            <div className="flex items-center gap-4 cursor-pointer">
+              <FontAwesomeIcon icon={faFileCircleCheck} className="text-2xl" />
+              <Link href="/projet" className="relative group z-50">
+                Projets
                 <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
               </Link>
-            </li>
+              <FontAwesomeIcon icon={isProjectOpen ? faChevronUp : faChevronDown} className="text-xl z-50" onClick={toggleProjectList} />
+            </div>
+            {isProjectOpen && (
+              <ol className="flex flex-col gap-2 ml-8 mt-3">
+                <li>
+                  <Link href="/projet/ydays" className="group relative z-50">
+                    Ydays
+                    <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/projet/web" className="group relative z-50">
+                    Web
+                    <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/projet/jeux" className="group relative z-50">
+                    Jeux
+                    <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
+                  </Link>
+                </li>
+              </ol>
+            )}
+          </li>
 
-            <li>
-              <div className="flex items-center gap-4 cursor-pointer">
-                <FontAwesomeIcon icon={faFileCircleCheck} className="text-2xl" />
-                <Link href="/projet" className="relative group z-50">
-                  Projets
-                  <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
-                </Link>
-                <FontAwesomeIcon icon={isProjectOpen ? faChevronUp : faChevronDown} className="text-xl z-50" onClick={toggleProjectList} />
-              </div>
-              {isProjectOpen && (
-                <ol className="flex flex-col gap-2 ml-8 mt-3">
-                  <li>
-                    <Link href="/projet/ydays" className="group relative z-50">
-                      Ydays
-                      <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/projet/web" className="group relative z-50">
-                      Web
-                      <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/projet/jeux" className="group relative z-50">
-                      Jeux
-                      <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
-                    </Link>
-                  </li>
-                </ol>
-              )}
-            </li>
+          <li className="flex items-center gap-4">
+            <FontAwesomeIcon icon={faCircleInfo} className="text-2xl" />
+            <Link href="/about" className="relative group z-50">
+              A propos
+              <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
+            </Link>
+          </li>
 
-            <li className="flex items-center gap-4">
-              <FontAwesomeIcon icon={faCircleInfo} className="text-2xl" />
-              <Link href="/about" className="relative group z-50">
-                A propos
-                <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
-              </Link>
-            </li>
-
-            <li className="flex items-center gap-4">
-              <FontAwesomeIcon icon={faFile} className="text-2xl" />
-              <Link href="/cv" className="relative group z-50">
-                CV
-                <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
-              </Link>
-            </li>
-          </ol>
-        </nav>
-      </div>
-    </ClientOnlyNavbar>
+          <li className="flex items-center gap-4">
+            <FontAwesomeIcon icon={faFile} className="text-2xl" />
+            <Link href="/cv" className="relative group z-50">
+              CV
+              <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
+            </Link>
+          </li>
+        </ol>
+      </nav>
+    </div>
   );
 }
