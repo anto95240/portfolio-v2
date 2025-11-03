4d388775fd1adfec2b1239a387d4be1c1b9735e0
anto95240 <richard33antoine@gmail.com>
2025-11-03 15:29:47 +0100

    UPDATE pwa + lien github

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import { NextConfig } from "next";
-import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
   // tes options Next.js normales
 };
 
-export default withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-})(nextConfig);
+export default nextConfig;
